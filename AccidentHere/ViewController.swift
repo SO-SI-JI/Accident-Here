789d98920c06527bbe03c6eb3e51691d4307e10c
elis0m <sominzzang615@gmail.com>
2021-12-14 21:08:08 +0900

    baseline 이상의 속도 시 배경화면 빨간색으로 변경, 코드 정리
    storyboard  auto layout 적용 및 수정
    불필요한 코드 제거
    baseline 이상의 속도일 때 배경화면 색상 red로 변경

--- AccidentHere/ViewController.swift
+++ AccidentHere/ViewController.swift
@@ -11,35 +11,33 @@ import CoreLocation
 class ViewController: UIViewController, CLLocationManagerDelegate {
     //MARK: Global Var's
     var locationManager: CLLocationManager = CLLocationManager()
-    var switchSpeed = "KPH"
     var startLocation:CLLocation!
     var lastLocation: CLLocation!
     var traveledDistance:Double = 0
-    var arrayMPH: [Double]! = []
     var arrayKPH: [Double]! = []
     
-    @IBOutlet var speedDisplay: UILabel!
-
-    @IBOutlet var headingDisplay: UILabel!
+    let baseline = 10.0
     
+    @IBOutlet var speedDisplay: UILabel!
     @IBOutlet var lonDisplay: UILabel!
     @IBOutlet var latDisplay: UILabel!
     @IBOutlet var distanceTraveled: UILabel!
     @IBOutlet var minSpeedLabel: UILabel!
     @IBOutlet var maxSpeedLabel: UILabel!
     @IBOutlet var avgSpeedLabel: UILabel!
-    
-    
+        
     //MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         minSpeedLabel.text = "0"
         maxSpeedLabel.text = "0"
+        
         // Ask for Authorisation from the User.
         self.locationManager.requestAlwaysAuthorization()
 
         // For use in foreground
         self.locationManager.requestWhenInUseAuthorization()
+        
         if CLLocationManager.locationServicesEnabled() {
             locationManager.delegate = self
             // 10미터 이내 정확도
@@ -48,7 +46,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             locationManager.startUpdatingLocation()
         }
     }
-
 
     // 1 mile = 5280 feet
     // Meter to miles = m * 0.00062137
@@ -66,104 +63,66 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         }
         if lastLocation != nil {
             traveledDistance += lastLocation.distance(from: locations.last!)
-            if switchSpeed == "MPH" {
-                if traveledDistance < 1609 {
-                    let tdF = traveledDistance / 3.28084
-                    distanceTraveled.text = (String(format: "%.1f Feet", tdF))
-                } else if traveledDistance > 1609 {
-                    let tdM = traveledDistance * 0.00062137
-                    distanceTraveled.text = (String(format: "%.1f Miles", tdM))
-                }
-            }
-            if switchSpeed == "KPH" {
-                if traveledDistance < 1609 {
-                    let tdMeter = traveledDistance
-                    distanceTraveled.text = (String(format: "%.0f Meters", tdMeter))
-                } else if traveledDistance > 1609 {
-                    let tdKm = traveledDistance / 1000
-                    distanceTraveled.text = (String(format: "%.1f Km", tdKm))
-                }
+            if traveledDistance < 1609 {
+                let tdMeter = traveledDistance
+                distanceTraveled.text = (String(format: "%.0f Meters", tdMeter))
+            } else if traveledDistance > 1609 {
+                let tdKm = traveledDistance / 1000
+                distanceTraveled.text = (String(format: "%.1f Km", tdKm))
             }
         }
         lastLocation = locations.last
-
     }
 
     func updateLocationInfo(latitude: CLLocationDegrees, longitude: CLLocationDegrees, speed: CLLocationSpeed, direction: CLLocationDirection) {
-        let speedToMPH = (speed * 2.23694)
         let speedToKPH = (speed * 3.6)
-        let val = ((direction / 22.5) + 0.5);
-        var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
-        let dir = arr[Int(val.truncatingRemainder(dividingBy: 16))]
-        //lonDisplay.text = coordinateString(latitude, longitude: longitude)
 
         lonDisplay.text = (String(format: "%.3f", longitude))
         latDisplay.text = (String(format: "%.3f", latitude))
-        if switchSpeed == "MPH" {
-            // Chekcing if speed is less than zero or a negitave number to display a zero
-            if (speedToMPH > 0) {
-                speedDisplay.text = (String(format: "%.0f mph", speedToMPH))
-                arrayMPH.append(speedToMPH)
-                let lowSpeed = arrayMPH.min()
-                let highSpeed = arrayMPH.max()
-                minSpeedLabel.text = (String(format: "%.0f mph", lowSpeed!))
-                maxSpeedLabel.text = (String(format: "%.0f mph", highSpeed!))
-                avgSpeed()
+        
+        // Checking if speed is less than zero
+        if (speedToKPH > 0) {
+            if (speedToKPH > baseline) {
+                self.view.backgroundColor = UIColor.red
             } else {
-                speedDisplay.text = "0 mph"
+                self.view.backgroundColor = nil
             }
+            
+            speedDisplay.text = (String(format: "%.0f km/h", speedToKPH))
+            arrayKPH.append(speedToKPH)
+            let lowSpeed = arrayKPH.min()
+            let highSpeed = arrayKPH.max()
+            minSpeedLabel.text = (String(format: "%.0f km/h", lowSpeed!))
+            maxSpeedLabel.text = (String(format: "%.0f km/h", highSpeed!))
+            avgSpeed()
+        } else {
+            speedDisplay.text = "0 km/h"
         }
-
-        if switchSpeed == "KPH" {
-            // Checking if speed is less than zero
-            if (speedToKPH > 0) {
-                speedDisplay.text = (String(format: "%.0f km/h", speedToKPH))
-                arrayKPH.append(speedToKPH)
-                let lowSpeed = arrayKPH.min()
-                let highSpeed = arrayKPH.max()
-                minSpeedLabel.text = (String(format: "%.0f km/h", lowSpeed!))
-                maxSpeedLabel.text = (String(format: "%.0f km/h", highSpeed!))
-                avgSpeed()
-                //                print("Low: \(lowSpeed!) - High: \(highSpeed!)")
-            } else {
-                speedDisplay.text = "0 km/h"
-            }
-        }
-
-        // Shows the N - E - S W
-        headingDisplay.text = "\(dir)"
-
     }
 
     func avgSpeed(){
-        if switchSpeed == "MPH" {
-            let speed:[Double] = arrayMPH
-            let speedAvg = speed.reduce(0, +) / Double(speed.count)
-            avgSpeedLabel.text = (String(format: "%.0f", speedAvg))
-            //print( votesAvg )
-        } else if switchSpeed == "KPH" {
-            let speed:[Double] = arrayKPH
-            let speedAvg = speed.reduce(0, +) / Double(speed.count)
-            avgSpeedLabel.text = (String(format: "%.0f", speedAvg))
-            //print( votesAvg
-        }
+        let speed:[Double] = arrayKPH
+        let speedAvg = speed.reduce(0, +) / Double(speed.count)
+        avgSpeedLabel.text = (String(format: "%.0f", speedAvg))
     }
-    @IBAction func restTripButton(_ sender: Any) {
-        arrayMPH = []
-        arrayKPH = []
-        traveledDistance = 0
-        minSpeedLabel.text = "0"
-        maxSpeedLabel.text = "0"
-        headingDisplay.text = "None"
-        speedDisplay.text = "0"
-        distanceTraveled.text = "0"
-        avgSpeedLabel.text = "0"
-    }
+    
     @IBAction func startTrip(_ sender: Any) {
         locationManager.startUpdatingLocation()
     }
+    
+    @IBAction func restTripButton(_ sender: Any) {
+        arrayKPH = []
+        traveledDistance = 0
+        speedDisplay.text = "-"
+        lonDisplay.text = "-"
+        latDisplay.text = "-"
+        distanceTraveled.text = "-"
+        minSpeedLabel.text = "-"
+        maxSpeedLabel.text = "-"
+        avgSpeedLabel.text = "-"
+    }
+    
     @IBAction func endTrip(_ sender: Any) {
         locationManager.stopUpdatingLocation()
     }
 }
-
